9c366dfc37f3644ac08b5cbc36389c2477e217dc
Keshab <keshabdas2003@gmail.com>
2025-09-12 08:10:05 +0530

    frontend init

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <h1 className=' bg-red-500 text-green-500'>Hi Keshab</h1>
+    </div>
+  )
+}
+
+export default App
